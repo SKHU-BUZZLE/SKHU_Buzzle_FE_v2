989ae26617c953b656b1593dc88b5c2d027ee18a
HarrySeop <harryseop@gmail.com>
2025-08-28 16:37:13 +0900

    🔨[#2] Fix: 오타 수정

--- packages/design-system/src/components/Test3.tsx
+++ packages/design-system/src/components/Test3.tsx
@@ -1,4 +1,4 @@
-export default function Test2() {
+export default function Test3() {
   return (
     <div className='animate-pulse rounded-xl border-2 border-dashed border-yellow-300 bg-yellow-50 p-6 text-xl text-yellow-900'>
       Test3 컴포넌트 입니다!
