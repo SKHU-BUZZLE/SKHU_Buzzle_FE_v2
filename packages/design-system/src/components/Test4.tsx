32b4a47e77b92956e1005c4c292c11e2d662e957
HarrySeop <harryseop@gmail.com>
2025-09-01 19:07:28 +0900

    ✅[#4] Test: CI/CD 테스트를 위한 테스트 컴포넌트 추가

---
+++ packages/design-system/src/components/Test4.tsx
@@ -0,0 +1,7 @@
+export default function Test4() {
+  return (
+    <div className='animate-pulse rounded-xl border-2 border-dashed border-green-300 bg-green-50 p-6 text-xl text-green-900'>
+      Test4 컴포넌트 입니다!
+    </div>
+  );
+}
--- packages/design-system/src/components/index.ts
+++ packages/design-system/src/components/index.ts
@@ -1,3 +1,5 @@
+// 테스트 컴포넌트
 export { default as Test1 } from './Test1';
 export { default as Test2 } from './Test2';
 export { default as Test3 } from './Test3';
+export { default as Test4 } from './Test4';
