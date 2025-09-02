0dc802749aceb23601435d34088ec1a281d6ce5d
HarrySeop <harryseop@gmail.com>
2025-09-02 21:09:42 +0900

    📦[#8] Chore: PR Preview CI/CD 세팅

---
+++ apps/buzzle/src/routes/router.tsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from 'react-router-dom';
+
+import App from '@/App';
+
+const basename = import.meta.env.BASE_URL;
+
+export const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <App />,
+      // children: [...]
+    },
+  ],
+  {
+    basename: basename,
+  },
+);
