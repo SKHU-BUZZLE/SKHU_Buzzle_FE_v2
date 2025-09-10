ce96f592cea95808bb9c9755bed3a9f0f4f81fd9
HarrySeop <harryseop@gmail.com>
2025-09-10 21:01:24 +0900

    ♻️ [#17] Refactor: App 컴포넌트 수정

--- apps/buzzle/src/App.tsx
+++ apps/buzzle/src/App.tsx
@@ -1,13 +1,10 @@
-import { Test1, Test2, Test3 } from '@buzzle/design';
+import { LAYOUT_PADDING } from '@constants/layout';
+import { Outlet } from 'react-router-dom';
 
 export default function App() {
   return (
-    <div>
-      <h1 className='text-center text-3xl font-bold'>Hello</h1>
-      <Test1 />
-      <Test2 />
-      <Test3 />
-      프리뷰 테스트 입니다!
-    </div>
+    <main className={LAYOUT_PADDING}>
+      <Outlet />
+    </main>
   );
 }
