bfab5de86d9d2e3008b727eacb5a3b767ab29d92
HarrySeop <harryseop@gmail.com>
2025-09-10 21:02:28 +0900

    ♻️ [#17] Refactor: 임시로 바텀네비게이션바 연결

--- apps/buzzle/src/layouts/RootFrame.tsx
+++ apps/buzzle/src/layouts/RootFrame.tsx
@@ -1,10 +1,13 @@
+import BottomNav from '@components/BottomNav';
+import { BOTTOM_NAV_PADDING, LAYOUT_MAX_WIDTH } from '@constants/layout';
 import { Outlet } from 'react-router-dom';
 
 export default function RootFrame() {
   return (
     <div className='bg-surface md:bg-white-600 min-h-dvh'>
-      <div className='bg-surface text-body mx-auto min-h-dvh max-w-768'>
+      <div className={`bg-surface text-body mx-auto min-h-dvh ${LAYOUT_MAX_WIDTH} ${BOTTOM_NAV_PADDING}`}>
         <Outlet />
+        <BottomNav />
       </div>
     </div>
   );
