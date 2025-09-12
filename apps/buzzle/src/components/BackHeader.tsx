c02ed49924ed64f195c8bdea6565c82e07bbfa5a
MyungJiwoo <1206jiwoo@gmail.com>
2025-09-12 17:42:51 +0900

    ✨ [#34] Feat: BackHeader 구현

---
+++ apps/buzzle/src/components/BackHeader.tsx
@@ -0,0 +1,67 @@
+import { ChevronIcon } from '@buzzle/design';
+import { type ReactNode, useEffect } from 'react';
+import { useBlocker, useNavigate } from 'react-router-dom';
+
+interface BackHeaderProps {
+  to: string;
+  preventBackNavigation?: boolean;
+  rightSlot?: ReactNode;
+}
+
+/** BackHeader
+ * @description 뒤로가기를 할 수 있는 BackHeader 컴포넌트입니다.
+ * @description 오른쪽에 들어올 아이템은 `rightSlot` prop을 통해 커스텀할 수 있습니다.
+ *
+ * @param {boolean} [props.preventBackNavigation=false] 라우터 전환 시 confirm으로 뒤로가기 차단 여부 (선택)
+ * @param {string} [props.to="/"] 뒤로가기 아이콘 클릭 시 이동할 경로 (필수)
+ * @param {ReactNode} [props.rightSlot] 헤더 우측에 렌더링할 요소 (선택)
+ *
+ * @example 1) 남은 하트 카운터 표시
+ * <BackHeader
+ *   to="/home"
+ *   rightSlot={<LifeCounter life={3} />}
+ * />
+ *
+ * @example 2) 삭제 버튼 표시
+ * <BackHeader
+ *   to="/note"
+ *   preventBackNavigation
+ *   rightSlot={
+ *     <DeleteIcon
+ *       className="text-black-300 dark:text-black-100 size-24 cursor-pointer"
+ *       onClick={() => console.log('삭제')}
+ *     />
+ *   }
+ * />
+ *
+ */
+export default function BackHeader({ to = '/', preventBackNavigation = false, rightSlot }: BackHeaderProps) {
+  const navigate = useNavigate();
+
+  // 현재 라우터에서 다른 모든 라우터로 이동을 차단
+  const blocker = useBlocker(preventBackNavigation);
+
+  useEffect(() => {
+    if (blocker.state === 'blocked') {
+      const proceed = window.confirm('페이지를 이동하시겠습니까?'); // todo: 모달로 대체할 예정
+      if (proceed) {
+        blocker.proceed(); // 이동 허용
+      } else {
+        blocker.reset(); // 이동 취소
+      }
+    }
+  }, [blocker]);
+
+  return (
+    <nav className='flex items-center justify-between gap-12 py-12'>
+      <ChevronIcon
+        aria-label='뒤로가기'
+        className='text-black-300 dark:text-black-100 size-28 cursor-pointer'
+        onClick={() => {
+          navigate(to);
+        }}
+      />
+      {rightSlot}
+    </nav>
+  );
+}
