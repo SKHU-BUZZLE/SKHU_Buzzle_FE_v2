5b296e59efee280bda3a844cc524bd1c0741ce5b
MyungJiwoo <1206jiwoo@gmail.com>
2025-09-12 16:00:38 +0900

    ✏️ [#20] Fix: 빈 className 삭제

--- apps/buzzle/src/layouts/RootFrame.tsx
+++ apps/buzzle/src/layouts/RootFrame.tsx
@@ -4,7 +4,7 @@ export default function RootFrame() {
   return (
     <div className='bg-backdrop min-h-dvh'>
       <div className='bg-surface text-body border-surface layout-max-width bottom-nav-padding mx-auto min-h-dvh border-x'>
-        <main className=''>
+        <main>
           <Outlet />
         </main>
       </div>
