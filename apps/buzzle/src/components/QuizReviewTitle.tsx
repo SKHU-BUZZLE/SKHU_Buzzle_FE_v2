cdad3c206a8ca43f32e8c24e9c40bda3c30c808d
MyungJiwoo <1206jiwoo@gmail.com>
2025-09-16 10:23:02 +0900

    ✨ [#44] Feat: QuizReviewTitle 구현

---
+++ apps/buzzle/src/components/QuizReviewTitle.tsx
@@ -0,0 +1,20 @@
+import { ChevronIcon } from '@buzzle/design';
+import { Link } from 'react-router-dom';
+
+export default function QuizReviewTitle({ title, quizId }: { title: string; quizId: number }) {
+  return (
+    <Link
+      className='flex items-center justify-between py-12 hover:opacity-80 active:brightness-80'
+      to={`/review/${quizId}`}
+    >
+      <div className='flex items-center gap-16'>
+        <p className='bg-primary-alpha-10 dark:bg-primary-alpha-20 text-primary-500 flex size-26 items-center justify-center rounded-full text-sm'>
+          Q
+        </p>
+        <p className='ds-text-normal ds-typ-body-1'>{title}</p>
+      </div>
+
+      <ChevronIcon className='ds-text-caption size-20 rotate-180' />
+    </Link>
+  );
+}
