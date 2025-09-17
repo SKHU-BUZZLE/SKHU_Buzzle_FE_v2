fb303a6624fc9a896992b1e3f9ef78b6e204b2a8
HarrySeop <harryseop@gmail.com>
2025-09-17 19:47:17 +0900

    ✨ [#59] Feat: 인증이 필요없는 API 제외 인터셉터 구현

--- apps/buzzle/src/apis/axiosInstance.ts
+++ apps/buzzle/src/apis/axiosInstance.ts
@@ -1,3 +1,4 @@
+import { useAuthStore } from '@stores/auth';
 import axios from 'axios';
 
 /**
@@ -18,4 +19,27 @@ export const axiosInstance = axios.create({
   headers: {
     'Content-Type': 'application/json',
   },
+});
+
+/** EXCLUDE_AUTH_URLS
+ * @description 인증이 필요 없는 API 경로입니다. 요청 인터셉터에서 해당 경로들은 accessToken을 헤더에 포함하지 않습니다.
+ */
+const EXCLUDE_AUTH_URLS = ['/oauth2/callback/kakao', '/kakao/token', '/token/access'];
+
+/** 요청 인터셉터
+ * 인증이 필요한 요청에는 Authorization 헤더에 accessToken을 자동으로 추가합니다.
+ */
+axiosInstance.interceptors.request.use((config) => {
+  const url = config.url ?? '';
+
+  // 로그인이 필요 없는 api는 토큰을 생략합니다.
+  const skip = EXCLUDE_AUTH_URLS.some((excluded) => url.startsWith(excluded));
+
+  if (!skip) {
+    const token = useAuthStore.getState().accessToken;
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+  }
+  return config;
 });
