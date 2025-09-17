a6289339bf1a9aff948d8c9b5e816ca682581e54
HarrySeop <harryseop@gmail.com>
2025-09-18 01:37:50 +0900

    ♻️ [#59] Refactor: 불필요한 태그 계층을 하나 줄임

--- apps/buzzle/src/layouts/RootFrame.tsx
+++ apps/buzzle/src/layouts/RootFrame.tsx
@@ -3,11 +3,9 @@ import { Outlet } from 'react-router-dom';
 export default function RootFrame() {
   return (
     <div className='ds-theme-bg-backdrop min-h-dvh'>
-      <div className='ds-theme-bg-base ds-text-normal ds-theme-border-base ds-layout-max-width mx-auto min-h-dvh border-x'>
-        <main>
-          <Outlet />
-        </main>
-      </div>
+      <main className='ds-theme-bg-base ds-text-normal ds-theme-border-base ds-layout-max-width mx-auto min-h-dvh border-x'>
+        <Outlet />
+      </main>
     </div>
   );
 }
