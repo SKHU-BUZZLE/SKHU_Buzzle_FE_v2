43229d592bc32cbe696435d95b9106626e0ad5be
MyungJiwoo <1206jiwoo@gmail.com>
2025-09-19 00:34:52 +0900

    💄 [#74] Style: BackHeaderFrame의 Outlet 사이즈를 전체 화면으로 수정

--- apps/buzzle/src/layouts/BackHeaderFrame.tsx
+++ apps/buzzle/src/layouts/BackHeaderFrame.tsx
@@ -4,10 +4,13 @@ import { Outlet } from 'react-router-dom';
 
 export default function BackHeaderFrame() {
   return (
-    <div className='ds-layout-padding h-full w-full'>
+    <div className='ds-layout-padding flex min-h-dvh w-full flex-col'>
       {/* 지우님이 해당 부분 동적 할당? 으로 수정해주시면 됩니다..!! */}
       <BackHeader rightSlot={<LifeCounter life={50} />} to='/home' />
-      <Outlet />
+      {/* Outlet 영역이 화면을 다 채우기 위해서 flex-1을 사용 / 여백을 위한 py 설정 */}
+      <main className='flex flex-1 flex-col py-16'>
+        <Outlet />
+      </main>
     </div>
   );
 }
