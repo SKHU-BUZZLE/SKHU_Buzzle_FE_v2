a2d933853778a49c7f64aaca4a42bdda0134a9b3
HarrySeop <harryseop@gmail.com>
2025-09-22 05:03:03 +0900

    ♻️ [#84] Refactor: 라이프 훅을 사용해서 프레임 수정

--- apps/buzzle/src/layouts/BackHeaderFrame.tsx
+++ apps/buzzle/src/layouts/BackHeaderFrame.tsx
@@ -1,12 +1,14 @@
 import { LifeCounter } from '@buzzle/design';
 import BackHeader from '@components/BackHeader';
+import { useLife } from '@hooks/useLife';
 import { Outlet } from 'react-router-dom';
 
 export default function BackHeaderFrame() {
+  const { data: life = 0 } = useLife();
+
   return (
     <div className='ds-layout-padding min-h-inherit flex w-full flex-1 flex-col'>
-      {/* 지우님이 해당 부분 동적 할당? 으로 수정해주시면 됩니다..!! */}
-      <BackHeader rightSlot={<LifeCounter life={50} />} to='/home' />
+      <BackHeader rightSlot={<LifeCounter life={life} />} to='/home' />
       {/* Outlet 영역이 화면을 다 채우기 위해서 flex-1을 사용 / 여백을 위한 py 설정 */}
       <section className='min-h-0 flex-1 overflow-y-auto'>
         <Outlet />
