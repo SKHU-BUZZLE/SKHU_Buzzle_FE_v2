41310d3b32926fe39831a19ac6a75b981492119b
HarrySeop <harryseop@gmail.com>
2025-09-21 15:21:09 +0900

    ♻️ [#84] Refactor: 레이아웃 수정

--- apps/buzzle/src/layouts/AppLayout.tsx
+++ apps/buzzle/src/layouts/AppLayout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom';
 
 export default function AppLayout() {
   return (
-    <div className='ds-layout-padding'>
+    <div className='ds-layout-padding min-h-inherit flex flex-1 flex-col'>
       <Outlet />
     </div>
   );
