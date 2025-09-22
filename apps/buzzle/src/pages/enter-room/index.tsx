e36a45b934ab2c634c412505165cc8ca1cf37bc8
MyungJiwoo <1206jiwoo@gmail.com>
2025-09-22 14:23:13 +0900

    ✨ [#80] Feat: 초대 코드가 유효한지 검사

--- apps/buzzle/src/pages/enter-room/index.tsx
+++ apps/buzzle/src/pages/enter-room/index.tsx
@@ -1,3 +1,4 @@
+import { validateInviteCode } from '@apis/multi';
 import { Button, FormField, KeyboardIcon } from '@buzzle/design';
 import { useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
@@ -5,6 +6,34 @@ import { useNavigate } from 'react-router-dom';
 export default function EnterRoomPage() {
   const navigate = useNavigate();
   const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleEnterRoom = async () => {
+    try {
+      const code = inputRef.current?.value ?? '';
+      if (!code) return; // 빈값 방지
+
+      const res = await validateInviteCode({ inviteCode: code });
+
+      if (res.data.statusCode === 200) {
+        // 유효한 초대코드라면 대기방으로 이동
+        navigate(`/multi-room/${code}/lobby`, {
+          state: { code },
+          replace: true, // 뒤로가기 시 코드 입력 페이지 안 보이게
+        });
+      } else {
+        // 유효하지 않은 초대 코드일 때
+        // TODO: 모달로 변경
+        // TODO: 메시지 수정
+        console.warn('[유효하지 않은 코드]', res);
+        alert('유효하지 않은 초대 코드입니다.');
+      }
+    } catch (err) {
+      // TODO: 모달로 변경
+      // TODO: 메시지 수정
+      console.error('[방 입장 실패]', err);
+      alert('초대 코드가 잘못되었거나 방이 존재하지 않습니다.');
+    }
+  };
 
   return (
     <div className='flex min-h-0 flex-1 flex-col gap-36'>
@@ -23,21 +52,7 @@ export default function EnterRoomPage() {
         rightSlot={<KeyboardIcon className='text-white-600 dark:text-white-900 size-24' />}
       />
 
-      <Button
-        className='mt-auto w-full'
-        onClick={async () => {
-          try {
-            // ! 서버에서 유효한 코드인지 먼저 확인 가능..?
-            const code = inputRef.current?.value;
-            navigate(`/multi-room/${code}/lobby`, {
-              state: { code },
-              replace: true, // 뒤로가기 시 코드 입력 페이지 안 보이게
-            });
-          } catch (err) {
-            console.error('[방 생성 실패]', err);
-          }
-        }}
-      >
+      <Button className='mt-auto w-full' onClick={handleEnterRoom}>
         입장하기
       </Button>
     </div>
