be91476f1456cd755675dc9a4bbcaf58d765cb3f
MyungJiwoo <1206jiwoo@gmail.com>
2025-09-25 15:25:15 +0900

    ♻️ [#99] Refactor: 404 페이지 UI 리팩토링

--- apps/buzzle/src/pages/not-found.tsx
+++ apps/buzzle/src/pages/not-found.tsx
@@ -1,8 +1,8 @@
 export default function NotFoundPage() {
   return (
-    <section className='space-y-8'>
+    <section className='flex flex-1 flex-col items-center justify-center'>
       <h1 className='ds-typ-heading-2 ds-text-strong'>404</h1>
-      <p className='ds-text-normal'>페이지를 찾을 수 없습니다.(임시)</p>
+      <p className='ds-text-normal'>페이지를 찾을 수 없습니다.</p>
     </section>
   );
 }
